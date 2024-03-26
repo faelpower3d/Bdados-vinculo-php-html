4fb0a468ea397258f369b5d087744fc0cef17ce2
Fatec <faelpower3d>
2024-03-26 19:13:21 -0300

    feito ate a conexao cm o banco falta o html

---
+++ index_pacient.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">   
+    <title>Paciente</title>
+</head>
+<body>
+    <p><h1>Paciente - Inclusão</h1>
+    <?php
+    //Verifica se o Formulario foi submetido
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        include ("clinica.php");
+        $nome = $_POST["nome"];
+        $endereco = $_POST["endereco"];
+        $numero = $_POST["numero"];
+        $complemento = $_POST["complemento"];
+        $bairro = $_POST["bairro"];
+        $cidade = $_POST["cidade"];
+        $estado = $_POST["estado"];
+        $cpf = $_POST["cpf"];
+        $rg = $_POST["rg"];
+        $telefone = $_POST["telefone"];
+        $celular = $_POST["celular"];        
+        $email = $_POST["email"];
+        $query = "INSERT INTO  paciente(nome,endereco,numero,complemento,
+        bairro,cidade,estado,cpf,rg,telefone,celular,email)
+        VALUES ('$nome','$endereco','$numero','$complemento',
+        '$bairro','$cidade','$estado','$cpf','$rg','$telefone','$celular','$email')";
+        $result = mysqli_query($con, $query);
+        if (mysqli_insert_id($con)) {
+            echo "Inclusão realizada com sucesso ! !";
+        }else{
+            echo "ERRO AO INSERIR OS DADOS : ".mysqli_connect_error();
+        }
+        mysqli_close($con);
+    }
+    ?>
+    <form method="post">
+    <label> Nome: </label>
+        <input type="text" size="80" maxlength="100" name="nome" required>
+        <label> CPF :</label>
+        <input type="text" size="11" maxlength="11" name="cpf" required>
+        <br><label> Endereço: </label>
+        <input type="text" size="80" maxlength="100" name="endereco" required>
+        <label> Número: </label>
+        <input type="number" maxlength="10" name="numero" required>
+        <label> Bairro: </label>
+        <input type="text" size="60" maxlength="60" name="bairro" required>
+        <label> Cidade: </label>
+        <input type="text" size="40" maxlength="80" name="cidade" required>
+        <label> Estado: </label>
+        <select name="estado">
+            <option value="SP"> São Paulo </option>
+            <option value="MG"> Minas Gerais </option>
+            <option value="PR"> Paraná </option>
+            <option value="RJ"> Rio de Janeiro  </option> </select>        
+        
+        <label> CRM  nº: </label>
+        <input type="text" size="20" maxlength="20" name="crm" required>
+        <label> Salario</label>
+        <input type="number" min="0" max="50000" name="salario" required>
+        <label> Celular: </label>
+        <input type="tel" maxlength="15" name="celular" placeholder="(XX)XXXXX-XXXX" required>
+        <label> Especialidade :</label>
+        <select name="cod_esp" >
+        
+
+    
+</body>
+</html>
